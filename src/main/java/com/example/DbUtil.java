157861192c896e6661933b71bde83f9cb632f3a6
srikanth <srikant.m@abc123.com>
2026-01-14 16:37:42 +0530

    change of endpoint1

--- src/main/java/com/example/DbUtil.java
+++ src/main/java/com/example/DbUtil.java
@@ -10,8 +10,8 @@ public class DbUtil {
 	static Connection con=null;
 	static String driver = "com.mysql.cj.jdbc.Driver";
 	static String url = "jdbc:mysql://lwplabs.ctu4464mwfyj.ap-south-1.rds.amazonaws.com:3306/lwplabs";
-	static String username = "master";
-	static String password = "welcome1";
+	static String username = "admin";
+	static String password = "shiva2026";
 	
 	static {
 		try {
